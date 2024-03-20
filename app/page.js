3ea0934aa3704e8fdef81dd4af156d536144c4a3
Gautam Raj <ashragautam25@gmail.com>
2024-03-20 15:16:58 +0530

    bug fix

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import OnBoard from "@/components/onBoard/OnBoard";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center">
+    <div className="h-screen flex items-center bg-gray-50">
       <OnBoard />
     </div>
   );
--- components/onBoard/OnBoard.js
+++ components/onBoard/OnBoard.js
@@ -3,7 +3,7 @@ import OnBoardRight from "./OnBoardRight";
 
 const OnBoard = () => {
   return (
-    <section className="flex bg-white/80 shadow-md w-full max-w-3xl mx-auto rounded-lg">
+    <section className="flex bg-white/80 shadow-lg w-full max-w-3xl mx-auto rounded-lg">
       <section className="w-2/5 border-r border-gray-700 font-grotesque flex flex-col justify-between">
         <OnBoardLeft />
       </section>
